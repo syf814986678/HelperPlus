9eaf2a7aab975d90702913640c0b8e68cecf919e
syf814986678 <814986678@qq.com>
2021-01-06 17:02:33 +0800

    START PROJECT

---
+++ helper-plus-web/src/main/java/com/shiyifan/HelperPlusWebApplication.java
@@ -0,0 +1,13 @@
+package com.shiyifan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperPlusWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelperPlusWebApplication.class, args);
+    }
+
+}
---
+++ helper-plus-web/src/test/java/com/shiyifan/HelperPlusWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiyifan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperPlusWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
